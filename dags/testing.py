271898de36702a36e58a035a2383cfe64d4c5d9c
femperox <ifilchukova1@gmail.com>
2022-02-16 17:20:58 +0300

    Проба Apache Airflow

--- dags/testing.py
+++ dags/testing.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.email import EmailOperator
 
 from datetime import datetime
 
@@ -19,4 +20,9 @@ with DAG('TESTING',
 
 		 task1 = DummyOperator(task_id = 'start')
 
-		 task1
+		 task2 = EmailOperator(task_id= 'email',
+							   to = "ifilchukova1@gmail.com",
+							   subject= 'testing',
+							   html_content= 'hello')
+
+		 task1 >> task2
