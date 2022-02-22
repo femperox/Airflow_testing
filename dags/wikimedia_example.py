55fc9085e1faf2f4fe2a76e562d84b25a2ff8daa
femperox <ifilchukova1@gmail.com>
2022-02-22 12:29:07 +0300

    ..

--- dags/wikimedia_example.py
+++ dags/wikimedia_example.py
@@ -6,6 +6,7 @@ https://dumps.wikimedia.org/other/pageviews/{year}/{year}-{month}/pageviews-{yea
 import airflow.utils.dates
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 from airflow.operators.email import EmailOperator
 from airflow.models import Variable
 
@@ -17,10 +18,22 @@ args = {
 
 with DAG( 'wikimedia_example',
           default_args = args,
-          schedule_interval = '@hourly'
+          schedule_interval = '@hourly',
+          catchup= False
         ) as dag:
 
-        get_data = BashOperator(
+        def get_data_python(execution_date):
+            year, month, day, hour, *_ = execution_date.timetuple()
+            print(year, month, day, hour)
+
+        def fetch_pageviews(pagenames):
+            result = dict.fromkeys(pagenames, 0)
+
+            path = Variable.get("Airflow_path")
+            with open(path+'/output/wikimedia'):
+                print('ok')
+
+        get_data_bash = BashOperator(
             task_id = 'get_data_by_Bash',
             bash_command = ( "mkdir -p /{{ '$AIRFLOW_HOME' }}/output/wikimedia && "  
                 "curl -o /{{ '$AIRFLOW_HOME' }}/output/wikimedia/wikipageviews.gz "
@@ -35,4 +48,23 @@ with DAG( 'wikimedia_example',
             )
         )
 
-        get_data
+        get_data_python = PythonOperator(
+            task_id='get_data_by_python',
+            python_callable = get_data_python,
+            provide_context= True
+        )
+
+        extract_gz = BashOperator(
+            task_id = 'extract_gz',
+            bash_command= 'gunzip --force /$AIRFLOW_HOME/output/wikimedia/wikipageviews.gz'
+        )
+
+        fetch_pageviews = PythonOperator(
+            task_id= 'fetch_pageviews',
+            python_callable= fetch_pageviews,
+            op_kwargs={
+                "pagenames": {"Google", "Amazon", "Apple", "Microsoft", "Facebook"}
+            }
+        )
+
+        get_data_bash >> get_data_python >> extract_gz >> fetch_pageviews
