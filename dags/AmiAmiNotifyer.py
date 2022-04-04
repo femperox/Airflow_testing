e4e2ba58334ea652736e1ee3ff978eac19a192af
femperox <ifilchukova1@gmail.com>
2022-04-04 13:20:17 +0300

    Работа над рассылкой

--- dags/AmiAmiNotifyer.py
+++ dags/AmiAmiNotifyer.py
@@ -4,9 +4,13 @@
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.models import Variable
+from airflow.operators.email import EmailOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.models import XCom
 
 from datetime import datetime
 import json
@@ -54,18 +58,100 @@ with DAG ( 'AmiAmiNotifyer',
             info['product_lnk'] = url[:-4]+product_url.get('href')
             infos.append(info)
 
-        pprint(infos)
-        #allFigures = allFigures.find_all('div', class_='product-area-1col')
-        #pprint(allFigures)
+        plsql_hook = PostgresHook(postgres_conn_id='my_conn_plsql')
+        conn = plsql_hook.get_conn()
+        cursor = conn.cursor()
 
-        #plsql_hook = PostgresHook(postgres_conn_id='my_conn_plsql')
-        #plsql_hook.get_conn()
+        for info in infos:
+            sql = "INSERT INTO FIGURES(NAME, PRICE_JPY, IMG_LNK, PRODUCT_LNK, CREATED_AT)" \
+                  f"VALUES ('{info['name'].replace(chr(39), chr(39)+chr(39))}', {info['price_jpy']}, '{info['img_link']}', '{info['product_lnk']}', '{datetime.now()}')" \
+                  "ON CONFLICT (NAME) DO NOTHING;"
+            cursor.execute(sql)
+            conn.commit()
+
+        conn.close()
+
+    def branching(**context):
+
+        plsql_hook = PostgresHook(postgres_conn_id='my_conn_plsql')
+        conn = plsql_hook.get_conn()
+        cursor = conn.cursor()
+
+        task_instance = context['task_instance']
+
+        sql = f"select count(name) from figures where created_at='{datetime.date(datetime.now())}'"
+        cursor.execute(sql)
+        result = (cursor.fetchall())[0][0]
+
+        task_instance.xcom_push(key='branching_amount', value=result)
+
+        if result > 0:
+            return 'task_make_ok_email'
+        else:
+            return 'task_make_notok_email'
 
 
+    def make_ok_email(**context):
 
+        plsql_hook = PostgresHook(postgres_conn_id='my_conn_plsql')
+        conn = plsql_hook.get_conn()
+        cursor = conn.cursor()
+
+        task_instance = context['task_instance']
+
+        sql = f"select * from figures where created_at='{datetime.date(datetime.now())}'"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+
+        content = """
+        <table width="AUTO" border="1" cellpadding="4" cellspacig="0">
+        <tr><th>Товар</th><th>Цена в йенах</th><th>Изображение</th>
+        """
+
+        for row in result:
+            content += '<tr>'
+            content += '<td>'+f'<a href="{row[4]}">{row[1]}"</a>'+'</td>'
+            content += '<td>'+str(row[2])+'</td>'
+            content += '<td>'+f'<img src="{row[3]}" alt="{row[1]}" title="{row[1]}"/>'+'</td>'
+            content += '</tr>'
+        content += '</table>'
+
+        pprint(content)
+
+        content = 'Привет, сегодня в подборке: \n' + content
+
+        task_instance.xcom_push(key='email', value=content)
+
+    def make_notok_email():
+        pass
 
     task_get_info = PythonOperator(
         task_id = 'task_get_info',
         python_callable= get_info
     )
-    task_get_info
+
+
+    task_branch = BranchPythonOperator(
+        task_id='task_branch',
+        python_callable= branching
+    )
+
+    task_make_ok_email = PythonOperator(
+        task_id='task_make_ok_email',
+        python_callable=make_ok_email
+    )
+
+    task_make_notok_email = PythonOperator(
+        task_id='task_make_notok_email',
+        python_callable=make_notok_email
+    )
+
+    task_send_email = EmailOperator(
+        task_id='task_send_email',
+        to = Variable.get('my_email'),
+        subject = 'GSC-расслыка',
+        html_content= "{{ task_instance.xcom_pull(key='email') }}",
+        trigger_rule='none_failed'
+    )
+
+    task_get_info >> task_branch >> [task_make_ok_email, task_make_notok_email] >> task_send_email
